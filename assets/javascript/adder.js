5901c3d05b9c3ffee8f3857b0bca34e06a9f27b9
Shehryar Ehtesham <se318@rutgers.edu>
2019-09-18 21:58:36 -0400

    fix for broken git

--- assets/javascript/adder.js
+++ assets/javascript/adder.js
@@ -1,11 +1,59 @@
-function add(num1, num2) {
-    var result = num1 + num2
-    return result;
+// 3 ways to write a stting
+
+var wordCheck = function (palindromeParam) {
+    //palindrome is a word that is spelled the same way forwards and backwards 
+    for (var i = 0; i < wordCheck.length; i++) {
+        if (wordCheck[i] !== [wordCheck.length - (i + 1)]) {    //if wordcheck
+            return false;
+        }
+    }
+    return true;
+};
+
+console.log(wordCheck("noon"));
+
+
+function checkPalindrome(myString) {
+    var isPalindrome = false;
+    var reverseString = myString.split("").reverse().join("");  //. split breaks each individual letter into an array 
+    //.reverse revereses the letters in the array and .join brings all the letters together into a word. 
+    if (myString === reverseString) {
+        isPalindrome = true;
+    }
+    return isPalindrome;
 }
 
-$("#submitBtn").on("click", function(){
-    var numOne = $("#number1-input").val();
-    var numTwo = $("#number2-input").val();
-    var result = add(numOne, numTwo);
-    $("#resultBox").text(result);
-})
+function isPalindrome(str) {
+    //  reverse the string
+    var strReverse = str.split("").reverse().join("");
+    // chek if str is the same as strReverse
+    return str.toLowerCase() === strReverse.toLowerCase;
+}
+
+
+
+ // Thing to remember: parseInt
+ var sum = 0;
+ sum = sum + 1; //sum 1
+ sum = sum + 2; //sum 3
+
+
+ sum += 1; // sum 4
+ sum += 2; // sum 6
+});
+
+ $(".gemImage").on("click", function () {
+        userScore += parseInt($(this).attr("value"));
+
+
+
+//write a fcuntion that tkes in the array of cards
+//sort cards by each suit 
+    // create a variable for each suit 
+// create for loop 
+
+var deck = suits; cards;
+var suits = ["clubs", "spades", "hearts", "diamonds"];
+var cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+
+var cardCounter = function(ar)
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,180 @@
 //Create 4 buttons (kybercrystals) 
-    //Each crystal genrates a random number (1-12)
+//Each crystal genrates a random number (1-12)
+
 //Create a varible that generates a random score between 19-120 at the start of the game. 
+//for this variable, create a function that has a min value of 19 and max value of 120. 
+//append random number to random tag in html
+
+//create variables for scores
+//creating random number for each jewel from 1-12
+
+//creating reset function
+//reset random # and log in back to html
+// reset all random numbers on gems
+//reset total to 0 and log it back into html
+
+//creating function for if won
+//create function for if loss
+
+//creat event when user clicks on blue gem
+//log new total back into total score in html
+//if user total equal to random number run winner function otherwise if total larger run loser function
+//repeat for all other gems 
+
+
+$(document).ready(function () {
+
+
+    //Create a varible that generates a random score between 19-120 at the start of the game. 
     //for this variable, create a function that has a min value of 19 and max value of 120. 
+    var randomNumberGenarator = function () {
+        var min = 19;
+        var max = 120;
+        var randomNumber = Math.floor(Math.random() * (max - min) + min);
+        return randomNumber;
+    };
+    var randomNumber = randomNumberGenarator();
+    console.log(randomNumber);
 
 
 
-$(document).ready(function() {
- 
+    //append random number to random tag in html
+    $("#randomNumber").append(randomNumber);
 
-// var randomNumber = randomNumberGenarator(19,120)  //Generates random number between 19-120
+    //delcare variables for score, wins and losses
+    var userScore = 0;
+    var wins = 0;
+    var losses = 0;
 
-var randomNumberGenarator = function(min, max) {
-    var randomNumber = Math.floor(Math.random() * (max - min) + min);
-    return randomNumber;
-};
-console.log(randomNumberGenarator(19,120));
+    $('#numberWins').text(wins);
+    $('#numberLosses').text(losses);
 
+    //create variables for scores to assign for each button
+    var crystScores = function () {
+        var min = 1;
+        var max = 12;
+        var score = Math.floor(Math.random() * (max - min) + min);
+        return score;
 
-function randomCrystalValue() {
-
-}
-
-function add(num1, num2) {
-    var result = num1 + num2
-    return result;
-}
-
-$("#submitBtn").on("click", function(){
-    var numOne = $("#number1-input").val();
-    var numTwo = $("#number2-input").val();
-    var result = add(numOne, numTwo);
-    $("#resultBox").text(result);
-})
+    };
+    console.log(crystScores());
 
 
-var userScore = 0; //User score starting at zero
-var wins = 0; //Wins start at 0
-var losses = 0; //Losses start at 0 
+    //Assingning each crystal the function: crystScores
+    var greenNum = crystScores();
+    var redNum = crystScores();
+    var yellowNum = crystScores();
+    var blueNum = crystScores();
+
+    //assinging buttons to each crystal variable 
+    $("#one").attr("value", blueNum)
+    $("#two").attr("value", greenNum)
+    $("#three").attr("value", redNum)
+    $("#four").attr("value", yellowNum)
+
+
+
+
+
+
+
+
+
+    //creating reset function
+
+    var reset = function () {
+        // Random Button gets new value
+        var randomNumber = Math.floor(Math.random() * (max - min) + min);
+        $('#randomNumber').text(randomNumber)
+
+        // Crystals get new values
+        var greenNum = crystScores();
+        var redNum = crystScores();
+        var yellowNum = crystScores();
+        var blueNum = crystScores();
+        //assinging new values to buttons
+        $("#one").attr("value", blueNum)
+        $("#two").attr("value", greenNum)
+        $("#three").attr("value", redNum)
+        $("#four").attr("value", yellowNum)
+
+
+        // User score goes to zero
+        userScore = 0
+        $('#finalTotal').text(userScore);
+        return userScore;
+
+    };
+
+    //adds the wins to the userScore
+    function winner() {
+        alert("You won!");
+        wins++;
+        $('#numberWins').text(wins);
+        reset();
+    }
+    //adds the losses to the userScore
+    function loser() {
+        alert("You lose!");
+        losses++;
+        $('#numberLosses').text(losses);
+        reset()
+    };
+
+    // 1. Click events for crystals
+
+    //click event for blue gem
+    $('#one').on('click', function () {
+        userScore = userScore + blueNum;
+        console.log("New userTotal= " + userScore);
+        $('#finalTotal').text(userScore);
+        if (userScore == randomNumber) {
+            winner();
+        }
+        else if (userTotal > randomNumber) {
+            loser();
+        }
+    });
+    //click event for green gem
+    $('#two').on('click', function () {
+        userScore = userScore + greenNum;
+        console.log("New userTotal= " + userScore);
+        $('#finalTotal').text(userScore);
+        //sets win/lose conditions
+        if (userScore == randomNumber) {
+            winner();
+        }
+        else if (userScore > randomNumber) {
+            loser();
+        }
+    })
+    //click event for red gem
+    $('#three').on('click', function () {
+        userScore = userScore + redNum;
+        console.log("New userTotal= " + userScore);
+        $('#finalTotal').text(userScore);
+        //sets win/lose conditions
+        if (userScore == randomNumber) {
+            winner();
+        }
+        else if (userScore > randomNumber) {
+            loser();
+        };
+    })
+    //click event for yellow gem
+    $('#four').on('click', function () {
+        userScore = userScore + yellowNum;
+        console.log("New userTotal= " + userScore);
+        $('#finalTotal').text(userScore);
+        //sets win/lose conditions
+        if (userScore == randomNumber) {
+            winner();
+        }
+        else if (userScore > randomNumber) {
+            loser();
+        };
+    })
 
 });
+
+
